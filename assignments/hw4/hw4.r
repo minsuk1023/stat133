7ef3c3975572083556a7e39218d69be94520f552
Minsuk Sung <minsuk1023@berkeley.edu>
2015-02-26 13:29:26 -0800

    hw4 done

--- assignments/hw4/hw4.r
+++ assignments/hw4/hw4.r
@@ -15,13 +15,15 @@
 #   element of <data.list>
 
 listLengths <- function(data.list) {
-
-    # your code here
-
+  listlength=c()
+  for (i in 1:length(data.list)){
+    listlength[i]=length(data.list[[i]])
+  }
+  return (listlength)
 }
 
-tryCatch(checkEquals(list.lengths.t, listLengths(ex3.test2)),
-         error=function(err) errMsg(err))
+list=list(c(1,2,3,4),c(1,2,3,4,5),c(1,2,3,4,5,6))
+listLengths(list)
 
 #### Function 2
 #### Implement the function "powers"
@@ -34,11 +36,16 @@ tryCatch(checkEquals(list.lengths.t, listLengths(ex3.test2)),
 # <x.powers> : A matrix of size [n x k] where the first column is x, the second column x^2, the third column x^4, etc.
 #              the column names should be : "x", "x^2", "x^3" etc.
 
-powers <- function(x, k){
-
+powers <- function(x, k){ 
+  original_x=x
+  for (i in 2:k) {
+    x=c(x,original_x^i)
+  }
+  a=matrix(x,nrow=length(original_x),ncol=k)
+  return (a)
 }
 
- 
+
 #### Function #3
 #### Implement the function "recipeConversion"
 
@@ -64,9 +71,27 @@ powers <- function(x, k){
 
 # Put your code here
 recipeConversion <- function(recipe){
-
+  recipe=data.frame(recipe,stringsAsFactors=F)
+  if (any(names(recipe)!=c("amount","unit","ingredient"))) {
+    warning("Wrong type data frame")
+    break
+  }
+  n=nrow(recipe)
+  for (i in 1:n){
+    if (any(recipe[i,2]=="cup"|recipe[i,2]=="cups")){
+      recipe[i,1]=recipe[i,1]*236.6
+      recipe[i,2]="ml"
+    }
+    else if (recipe[i,2]=="oz"){
+      recipe[i,1]=recipe[i,1]* 28.3
+      recipe[i,2]="gr"
+    }
+  }
+  return (recipe)
 }
 
+df=data.frame(amount=c(1,2,3),unit=c("cup","cups","oz"),ingredient=c("a","b","c"))
+df=data.frame(amount=c(1,2,3),unit=c("cup","cups","oz"),ingredient=c("a","b","c"),stringsAsFactors=F)
 
 #### Function #4a
 # Implement the function "bootstrapVarEst"
@@ -77,8 +102,12 @@ recipeConversion <- function(recipe){
 # Output variable:
 # <boot.sigma.est> : Bootstrap estimate for the variance of the sample mean (see lecture notes)
 
-bootstrapVarEst <- function(x){
-
+bootstrapVarEst <- function(x, B){
+  mu=c()
+  for (i in 1:B){
+    mu[i]=mean(sample(x,size=length(x),replace=TRUE))
+  }
+  return (var(mu))
 }
 
 #### Function #4b
@@ -90,9 +119,14 @@ bootstrapVarEst <- function(x){
 # Output variable:
 # <jack.sigma.est> : Jackknife estimate for the variance of the sample mean (see lecture notes)
 
-jackknifeVarEst <- fuction(x){
-
+jackknifeVarEst <- function(x){
+  mu=c()
+  for (i in 1:length(x)){
+    mu[i]=mean(x[-i])
+  }
+  return (var(mu))
 }
+x=c(1,2,3,4)
 
 #### Function #4c
 #### Implement the function "samplingVarEst"
@@ -106,8 +140,13 @@ jackknifeVarEst <- fuction(x){
 
 # Note: this function calls the previous two functions.
 
-samplingVarEst <- function(  ){
-
+samplingVarEst <- function(x,type="bootstrap"){
+  if (type=="bootstrap"){
+    return (bootstrapVarEst(x,5000))
+  }
+  else if (type=="jackknife"){
+    return (jackknifeVarEst(x))
+  }
 }
 
 
